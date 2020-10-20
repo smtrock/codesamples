74660b2fa54615788de5ee51f5843943eb10159d
marcus <mdfern@outlook.com>
2020-10-20 12:02:39 -0400

    removed __pycache__ folders and .idea folder

--- djangoTuts/mysite/polls/views.py
+++ djangoTuts/mysite/polls/views.py
@@ -1,3 +1,4 @@
+from datetime import timezone
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
@@ -11,8 +12,12 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions 
+        (not including those set to be published 
+        in the future).
+        """
+        return Question.objects.filter(pub_date_lte=timezone.now()).order_by('pub_date')[:5]
 
 
 class DetailView(generic.DetailView):

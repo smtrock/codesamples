f062362c69e5852ef4256429048843adeb9ccc29
marcus <mdfern@outlook.com>
2020-10-23 22:45:53 -0400

    worked through part 2-5

--- djangoTuts/mysite/polls/tests.py
+++ djangoTuts/mysite/polls/tests.py
@@ -1,23 +1,19 @@
-import datetime
-from django.http import response
-
-from django.test import TestCase
-from django.utils import timezone
+from datetime import timedelta,datetime
 from django.urls import reverse
-
+from django.test import TestCase
+from django.utils import timezone as tz
+from django.views import generic
 from .models import Question
-
-import datetime
-# Create your tests here.
 
 class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
-        """ 
-        was_published_recently() returns questions whose pu_date is in the future
         """
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date = time)
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = tz.now() + timedelta(days=30)
+        future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
 
     def test_was_published_recently_with_old_question(self):
@@ -25,7 +21,7 @@ class QuestionModelTests(TestCase):
         was_published_recently() returns False for questions whose pub_date
         is older than 1 day.
         """
-        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        time = tz.now() - timedelta(days=1, seconds=1)
         old_question = Question(pub_date=time)
         self.assertIs(old_question.was_published_recently(), False)
 
@@ -34,9 +30,17 @@ class QuestionModelTests(TestCase):
         was_published_recently() returns True for questions whose pub_date
         is within the last day.
         """
-        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        time = tz.now() - timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 def create_question(question_text, days):
     """
@@ -44,8 +48,9 @@ def create_question(question_text, days):
     given number of `days` offset to now (negative for questions published
     in the past, positive for questions that have yet to be published).
     """
-    time = timezone.now() + datetime.timedelta(days=days)
+    time = tz.now() + timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
+
 
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
@@ -103,24 +108,3 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
-
-class QuestionDetailViewTests(TestCase):
-    def test_future_question(self):
-        """
-        The detail view of a question with a pub_date in the future
-        returns a 404 not found.
-        """
-        future_question = create_question(question_text='Future question.', days=5)
-        url = reverse('polls:detail', args=(future_question.id,))
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
-
-    def test_past_question(self):
-        """
-        The detail view of a question with a pub_date in the past
-        displays the question's text.
-        """
-        past_question = create_question(question_text='Past Question.', days=-5)
-        url = reverse('polls:detail', args=(past_question.id,))
-        response = self.client.get(url)
-        self.assertContains(response, past_question.question_text)
--- djangoTuts/mysite/polls/views.py
+++ djangoTuts/mysite/polls/views.py
@@ -1,9 +1,8 @@
-from datetime import timezone
 from django.http import HttpResponseRedirect
+from django.utils import timezone
+from django.views import generic
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.views import generic
-
 from .models import Choice, Question
 
 
@@ -17,7 +16,7 @@ class IndexView(generic.ListView):
         (not including those set to be published 
         in the future).
         """
-        return Question.objects.filter(pub_date_lte=timezone.now()).order_by('pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
